5457ed167d3e605a0e9de15304b6496c3c3679a3
Michael Czechowski <mail@dailysh.it>
2023-09-05 01:09:29 +0200

    change mp-hermes to port 6000

--- mp-hermes/vite.config.ts
+++ mp-hermes/vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
         }
     },
     server: {
+        port: 6000,
         proxy: {
             '/api': {
                 target: 'http://localhost:5000/mp-pegasos', // Replace with your API server URL
